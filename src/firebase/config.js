30dcb6e4844a54decc5764cf0def3550ad1c4381
ibimina <96787210+ibimina@users.noreply.github.com>
2022-09-27 15:21:04 +0100

    react app reset,added firebase config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore, Timestamp, Timestamp } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAM1_IH119PaWefFZGFAja00_MzfNHbHKs",
+  authDomain: "ibtours.firebaseapp.com",
+  projectId: "ibtours",
+  storageBucket: "ibtours.appspot.com",
+  messagingSenderId: "232619070764",
+  appId: "1:232619070764:web:3c053ad6d5af7d5acb0889",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+const db = getFirestore();
+
+const timeStamp = Timestamp;
+
+return { db, auth, timeStamp };
